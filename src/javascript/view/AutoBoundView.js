65a7c3a5143d8674ac2ec16d92b59b720acfafb7
Bo Gotthardt <mail@gotthardt.bo>
2013-10-24 20:30:12 +0200

    Auto binding experiment.

---
+++ src/javascript/view/AutoBoundView.js
@@ -0,0 +1,29 @@
+define(function (require) {
+    "use strict";
+    var $ = require("jquery");
+    var _ = require("underscore");
+    var Backbone = require("backbone");
+    var Marionette = require("marionette");
+
+    return Marionette.ItemView.extend({
+        constructor: function () {
+            Marionette.ItemView.prototype.constructor.apply(this, arguments);
+            var scope = this;
+
+            var autoBind = this.autoBind;
+            if (autoBind) {
+                this.listenTo(this, "render", function () {
+                    var bindings = {};
+                    var prefix = _.isString(autoBind) ? autoBind : "#";
+
+                    _.each(scope.model.attributes, function (value, name) {
+                        var selector = prefix + name;
+                        bindings[selector] = name;
+                    });
+
+                    scope.stickit(scope.model, bindings);
+                });
+            }
+        }
+    });
+});
