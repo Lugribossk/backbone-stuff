bd380f0de29bd51b0e865c2c1c429ea35c468f6e
Bo Gotthardt <mail@gotthardt.bo>
2013-12-03 16:25:08 +0100

    Moved Karma settings to their own file so they will work with IntelliJ13's Karma run config.

--- grunt/test.js
+++ grunt/test.js
@@ -33,37 +33,16 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks("grunt-karma");
     grunt.config.set("karma", {
         options: {
-            frameworks: ["requirejs", "jasmine"],
-            // Disable the default html2js preprocessor as it screws up the Handlebars HTML files.
-            preprocessors: {},
-            files: [
-                "src/javascript/require.config.js",
-                "test/test-main.js",
-                // Serve all the code, but don't include it as script tags. RequireJS will load them.
-                {pattern: "src/javascript/**/*", included: false},
-                {pattern: "bower_components/**/*.js", included: false},
-                {pattern: "test/**/*", included: false}
-            ]
+            configFile: "karma.conf.js"
         },
         unit: {
-            browsers: ["Chrome"]
+            browsers: ["Chrome"],
+            singleRun: false,
+            preprocessors: {},
+            reporters: ["progress"]
         },
         ci: {
-            browsers: ["PhantomJS"],
-            singleRun: true,
-            // Randomize the port in case several CI jobs are running at the same time.
-            port: 10000 + Math.round(Math.random() * 1000),
-            preprocessors: {
-                "src/javascript/**/*.js": ["coverage"]
-            },
-            reporters: ["progress", "junit", "coverage"],
-            junitReporter: {
-                outputFile: "target/test-results.xml"
-            },
-            coverageReporter: {
-                type: "html",
-                dir: "target/coverage"
-            }
+            // Empty on purpose, as we're just reusing the karma.conf settings.
         }
     });
 
